4449b13fc3daebc801b289a97bbed502fc446701
wohlfea <wohlfea@gmail.com>
2015-10-29 13:20:31 -0700

    connect html to javascript so alerts can be displayed on the page

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
+var ans1 = document.getElementById('answer1');
+var ans2 = document.getElementById('answer2');
+var ans3 = document.getElementById('answer3');
+var ans4 = document.getElementById('answer4');
+var ans5 = document.getElementById('answer5');
+var ans6 = document.getElementById('answer6');
+var ttlscore = document.getElementById('finalScore');
+
 //Javascript practice-prompt & alert//
 
 var name = prompt('What is your name?');
@@ -24,39 +32,48 @@ function intro() {
 function q1(){
   var question1 = prompt('Did I grow up in the US? (YES or NO)').toUpperCase();
   if (question1 === ('YES' || 'Y')) {
-    alert('No, I grew up in Japan.');
+    ans1.innerHTML = 'No, I grew up in Japan.';
+    // alert('No, I grew up in Japan.');
   } else if (question1 === ('NO' || 'N')) {
-    alert('Yes, you\'re correct!');
+    ans1.innerHTML = 'Yes, you\'re correct!';
+    // alert('Yes, you\'re correct!');
     console.log(question1 + ' : the user selected a correct answer.');
     score += 1;
   } else {
-    alert('That\'s not an option.');
+    ans1.innerHTML = 'That\'s not an option.';
+    //alert('That\'s not an option.');
   }
 }
 
 function q2(){
   var question2 = prompt('Do I like ice cream sandwiches? (YES or NO)').toUpperCase();
   if (question2 === ('YES' || 'Y')) {
-    alert('Yes, I love ice cream sandwiches!');
+    ans2.innerHTML = 'Yes, I love ice cream sandwiches!';
+    // alert('Yes, I love ice cream sandwiches!');
     console.log(question2 + ' : the user selected a correct answer.');
     score += 1;
   } else if (question2 === ('NO' || 'N')) {
-    alert('No, you\'re incorrect.');
+    ans2.innerHTML = 'No, you\'re incorrect.';
+    // alert('No, you\'re incorrect.');
   } else {
-    alert('That\'s not an option.');
+    ans2.innerHTML = 'That\'s not an option.';
+    // alert('That\'s not an option.');
   }
 }
 
 function q3(){
   var question3 = prompt('Did I move from Hawaii? (YES or NO)').toUpperCase();
   if (question3 === ('YES' || 'Y')) {
-    alert('Yes, I did move from Hawaii!');
+    ans3.innerHTML = 'Yes, I did move from Hawaii!';
+    // alert('Yes, I did move from Hawaii!');
     console.log(question3 + ' : the user selected a correct answer.');
     score += 1;
   } else if (question3 === ('NO' || 'N')) {
-    alert('No, you\'re incorrect.');
+    ans3.innerHTML = 'No, you\'re incorrect.';
+    // alert('No, you\'re incorrect.');
   } else {
-    alert('That\'s not an option.');
+    ans3.innerHTML = 'That\'s not an option.';
+    // alert('That\'s not an option.');
   }
 }
 
@@ -67,12 +84,14 @@ function q4() {
   var askQuestion4 = 'What is my favorite number?';
   var userAnswer4 = parseInt(prompt(askQuestion4));
   while (userAnswer4 !== correctAnswer) {
-  alert('That is not my favorite number. hint- between 5 and 9');
+  ans4.innerHTML = 'That is not my favorite number. hint- between 5 and 9'
+  // alert('That is not my favorite number. hint- between 5 and 9');
   console.log('the user typed:' + ' ' + 'number' + ' ' + userAnswer4);
   userAnswer4 = parseInt(prompt(askQuestion4))
     if(userAnswer4 === correctAnswer){
       console.log('the user selected a correct answer:' + ' ' + correctAnswer);
-      alert('Yes, that is correct!');
+      ans4.innerHTML = 'Yes, that is correct!'
+      // alert('Yes, that is correct!');
     }
   }
 }
@@ -81,13 +100,16 @@ function q4() {
 function q5() {
   var question5 = prompt('Do you think I like sea otters? (YES or NO)').toUpperCase();
   if (question5 === ('YES' || 'Y')) {
-    alert('Yes, I love sea otters, and I think they are cute.');
+    ans5.innerHTML = 'Yes, I love sea otters, and I think they are cute.';
+    // alert('Yes, I love sea otters, and I think they are cute.');
     console.log(question5 + ' : the user selected a correct answer.');
     score += 1;
   } else if (question5 === ('NO' || 'N')) {
-    alert('No, you\'re incorrect.');
+    ans5.innerHTML = 'No, you\'re incorrect.';
+    // alert('No, you\'re incorrect.');
   } else {
-    alert('That\'s not an option.');
+    ans5.innerHTML = 'That\'s not an option.';
+    // alert('That\'s not an option.');
   }
 }
 
@@ -97,12 +119,14 @@ function q6() {
   var askQuestion6 = 'How long did I live in Hawaii?';
   var userAnswer6 = parseInt(prompt(askQuestion6));
     while (userAnswer6 !== correctAnswer6) {
-    alert('That\'s incorrect. hint- more than a decay, less than 15.');
+    ans6.innerHTML = 'That\'s incorrect. hint- more than a decay, less than 15.';
+    // alert('That\'s incorrect. hint- more than a decay, less than 15.');
     console.log('the user typed:' + ' ' + 'number' + ' ' + userAnswer6);
     userAnswer6 = parseInt(prompt(askQuestion6))
       if(userAnswer6 === correctAnswer6){
         console.log('the user selected a correct answer:' + ' ' + correctAnswer6);
-        alert('Yes, that is correct!');
+        ans6.innerHTML = 'Yes, that is correct!';
+        // alert('Yes, that is correct!');
     }
   }
 }
@@ -114,4 +138,5 @@ q3();
 q4();
 q5();
 q6();
-alert('You got' + ' ' + score + ' correct answers out of 6 questions!' );
+ttlscore.innerHTML = 'You got' + ' ' + score + ' correct answers out of 6 questions!';
+// alert('You got' + ' ' + score + ' correct answers out of 6 questions!' );
