991bacb37ba741f1dd036f00625c40559d163cdd
Rick Tesmond <tesmonrd@gmail.com>
2015-10-30 15:54:21 -0700

    Added classes in the functions

--- game.js
+++ game.js
@@ -33,14 +33,17 @@ function q1(){
   var question1 = prompt('Did I grow up in the US? (YES or NO)').toUpperCase();
   if (question1 === ('YES' || 'Y')) {
     ans1.innerHTML = 'No, I grew up in Japan.';
+    ans1.className = "red";
     // alert('No, I grew up in Japan.');
   } else if (question1 === ('NO' || 'N')) {
     ans1.innerHTML = 'Yes, you\'re correct! <br /> <img style= "width: 200px"; src="img/osakajpn.jpg" alt="Osaka, Japan">';
+    ans1.className= "green";
     // alert('Yes, you\'re correct!');
     console.log(question1 + ' : the user selected a correct answer.');
     score += 1;
   } else {
     ans1.innerHTML = 'That\'s not an option.';
+    ans1.className= "red";
     //alert('That\'s not an option.');
   }
 }
@@ -49,14 +52,17 @@ function q2(){
   var question2 = prompt('Do I like ice cream sandwiches? (YES or NO)').toUpperCase();
   if (question2 === ('YES' || 'Y')) {
     ans2.innerHTML = 'Yes, I love ice cream sandwiches! <br /> <img style="width: 200px"; src="img/icecream.jpg" alt="Ice cream">';
+    ans2.className = "green";
     // alert('Yes, I love ice cream sandwiches!');
     console.log(question2 + ' : the user selected a correct answer.');
     score += 1;
   } else if (question2 === ('NO' || 'N')) {
     ans2.innerHTML = 'No, you\'re incorrect.';
+    ans2.className = "red";
     // alert('No, you\'re incorrect.');
   } else {
     ans2.innerHTML = 'That\'s not an option.';
+    ans2.className = "red";
     // alert('That\'s not an option.');
   }
 }
@@ -65,14 +71,17 @@ function q3(){
   var question3 = prompt('Did I move from Hawaii? (YES or NO)').toUpperCase();
   if (question3 === ('YES' || 'Y')) {
     ans3.innerHTML = 'Yes, I did move from Hawaii! <br /> <img style= "width: 200px"; src="img/hawaii.jpg" alt="Hawaii">';
+    ans3.className = "green";
     // alert('Yes, I did move from Hawaii!');
     console.log(question3 + ' : the user selected a correct answer.');
     score += 1;
   } else if (question3 === ('NO' || 'N')) {
     ans3.innerHTML = 'No, you\'re incorrect.';
+    ans3.className = "red";
     // alert('No, you\'re incorrect.');
   } else {
     ans3.innerHTML = 'That\'s not an option.';
+    ans3.className = "red";
     // alert('That\'s not an option.');
   }
 }
@@ -91,6 +100,7 @@ function q4() {
     // if(userAnswer4 === correctAnswer){
       console.log('the user selected a correct answer:' + ' ' + correctAnswer);
       ans4.innerHTML = 'Yes, that is correct! <br /> <img style= "width: 200px"; src="img/numbereight.jpg" alt="Number Eight">';
+      ans4.className = "green";
         // alert('Yes, that is correct!');
     // }
   // }
@@ -101,14 +111,17 @@ function q5() {
   var question5 = prompt('Do you think I like sea otters? (YES or NO)').toUpperCase();
   if (question5 === ('YES' || 'Y')) {
     ans5.innerHTML = 'Yes, I love sea otters, and I think they are cute. <br /> <img style="width: 200px"; src="img/seaotter.jpg" alt="Sea Otter">';
+    ans5.className = "green";
     // alert('Yes, I love sea otters, and I think they are cute.');
     console.log(question5 + ' : the user selected a correct answer.');
     score += 1;
   } else if (question5 === ('NO' || 'N')) {
     ans5.innerHTML = 'No, you\'re incorrect.';
+    ans5.className = "red";
     // alert('No, you\'re incorrect.');
   } else {
     ans5.innerHTML = 'That\'s not an option.';
+    ans5.className = "red";
     // alert('That\'s not an option.');
   }
 }
@@ -124,8 +137,9 @@ function q6() {
     console.log('the user typed:' + ' ' + 'number' + ' ' + userAnswer6);
     userAnswer6 = parseInt(prompt(askQuestion6)) }
       // if(userAnswer6 === correctAnswer6){
-        console.log('the user selected a correct answer:' + ' ' + correctAnswer6);
-        ans6.innerHTML = 'Yes, that is correct!<br /> <img style="width: 200px"; src="img/livedinhawaii11yrs.jpg" alt="Lived in hawaii">';
+    console.log('the user selected a correct answer:' + ' ' + correctAnswer6);
+    ans6.innerHTML = 'Yes, that is correct!<br /> <img style="width: 200px"; src="img/livedinhawaii11yrs.jpg" alt="Lived in hawaii">';
+    ans6.className = "green";
         // alert('Yes, that is correct!');
     // }
   // }
