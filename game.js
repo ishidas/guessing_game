85d024e8cc14e65cbe9c635223ad683f16713f99
Rick Tesmond <tesmonrd@gmail.com>
2015-10-30 15:30:27 -0700

    Added time, photos, punchline, modified while loop

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ function q1(){
     ans1.innerHTML = 'No, I grew up in Japan.';
     // alert('No, I grew up in Japan.');
   } else if (question1 === ('NO' || 'N')) {
-    ans1.innerHTML = 'Yes, you\'re correct!';
+    ans1.innerHTML = 'Yes, you\'re correct! <br /> <img style= "width: 200px"; src="img/osakajpn.jpg" alt="Osaka, Japan">';
     // alert('Yes, you\'re correct!');
     console.log(question1 + ' : the user selected a correct answer.');
     score += 1;
@@ -48,7 +48,7 @@ function q1(){
 function q2(){
   var question2 = prompt('Do I like ice cream sandwiches? (YES or NO)').toUpperCase();
   if (question2 === ('YES' || 'Y')) {
-    ans2.innerHTML = 'Yes, I love ice cream sandwiches!';
+    ans2.innerHTML = 'Yes, I love ice cream sandwiches! <br /> <img style="width: 200px"; src="img/icecream.jpg" alt="Ice cream">';
     // alert('Yes, I love ice cream sandwiches!');
     console.log(question2 + ' : the user selected a correct answer.');
     score += 1;
@@ -64,7 +64,7 @@ function q2(){
 function q3(){
   var question3 = prompt('Did I move from Hawaii? (YES or NO)').toUpperCase();
   if (question3 === ('YES' || 'Y')) {
-    ans3.innerHTML = 'Yes, I did move from Hawaii!';
+    ans3.innerHTML = 'Yes, I did move from Hawaii! <br /> <img style= "width: 200px"; src="img/hawaii.jpg" alt="Hawaii">';
     // alert('Yes, I did move from Hawaii!');
     console.log(question3 + ' : the user selected a correct answer.');
     score += 1;
@@ -84,23 +84,23 @@ function q4() {
   var askQuestion4 = 'What is my favorite number?';
   var userAnswer4 = parseInt(prompt(askQuestion4));
   while (userAnswer4 !== correctAnswer) {
-  ans4.innerHTML = 'That is not my favorite number. hint- between 5 and 9'
-  // alert('That is not my favorite number. hint- between 5 and 9');
-  console.log('the user typed:' + ' ' + 'number' + ' ' + userAnswer4);
-  userAnswer4 = parseInt(prompt(askQuestion4))
-    if(userAnswer4 === correctAnswer){
+    ans4.innerHTML = 'That is not my favorite number. hint- between 5 and 9'
+    // alert('That is not my favorite number. hint- between 5 and 9');
+    console.log('the user typed:' + ' ' + 'number' + ' ' + userAnswer4);
+    userAnswer4 = parseInt(prompt(askQuestion4)) }
+    // if(userAnswer4 === correctAnswer){
       console.log('the user selected a correct answer:' + ' ' + correctAnswer);
-      ans4.innerHTML = 'Yes, that is correct!'
-      // alert('Yes, that is correct!');
-    }
-  }
+      ans4.innerHTML = 'Yes, that is correct! <br /> <img style= "width: 200px"; src="img/numbereight.jpg" alt="Number Eight">';
+        // alert('Yes, that is correct!');
+    // }
+  // }
 }
 
 //adding question 5
 function q5() {
   var question5 = prompt('Do you think I like sea otters? (YES or NO)').toUpperCase();
   if (question5 === ('YES' || 'Y')) {
-    ans5.innerHTML = 'Yes, I love sea otters, and I think they are cute.';
+    ans5.innerHTML = 'Yes, I love sea otters, and I think they are cute. <br /> <img style="width: 200px"; src="img/seaotter.jpg" alt="Sea Otter">';
     // alert('Yes, I love sea otters, and I think they are cute.');
     console.log(question5 + ' : the user selected a correct answer.');
     score += 1;
@@ -122,21 +122,27 @@ function q6() {
     ans6.innerHTML = 'That\'s incorrect. hint- more than a decay, less than 15.';
     // alert('That\'s incorrect. hint- more than a decay, less than 15.');
     console.log('the user typed:' + ' ' + 'number' + ' ' + userAnswer6);
-    userAnswer6 = parseInt(prompt(askQuestion6))
-      if(userAnswer6 === correctAnswer6){
+    userAnswer6 = parseInt(prompt(askQuestion6)) }
+      // if(userAnswer6 === correctAnswer6){
         console.log('the user selected a correct answer:' + ' ' + correctAnswer6);
-        ans6.innerHTML = 'Yes, that is correct!';
+        ans6.innerHTML = 'Yes, that is correct!<br /> <img style="width: 200px"; src="img/livedinhawaii11yrs.jpg" alt="Lived in hawaii">';
         // alert('Yes, that is correct!');
-    }
-  }
+    // }
+  // }
 }
 
 intro();
-q1();
-q2();
-q3();
-q4();
-q5();
-q6();
+window.setTimeout(q1, 1000);
+window.setTimeout(q2, 1050);
+window.setTimeout(q3, 1100);
+window.setTimeout(q4, 1150);
+window.setTimeout(q5, 1200);
+window.setTimeout(q6, 1250);
+// q1();
+// q2();
+// q3();
+// q4();
+// q5();
+// q6();
 ttlscore.innerHTML = 'You got' + ' ' + score + ' correct answers out of 6 questions!';
 // alert('You got' + ' ' + score + ' correct answers out of 6 questions!' );
